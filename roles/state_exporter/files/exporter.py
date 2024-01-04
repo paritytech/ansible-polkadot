3a5af1946743314556590aca9905b96d519afb49
kogeler <roman.gavrilov@parity.io>
2024-01-04 11:54:09 +0200

    the state-exporter role was fixed
    Signed-off-by: kogeler <roman.gavrilov@parity.io>

--- roles/state_exporter/files/exporter.py
+++ roles/state_exporter/files/exporter.py
@@ -122,21 +122,23 @@ def update_metrics():
 def parse_session_key(dir):
     # variants of key prefixes in the right order
     key_formats = (
-        ['6772616e', '62616265', '696d6f6e', '70617261', '61756469'],
-        ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469'])
+        ['6772616e', '62616265', '696d6f6e', '70617261', '61756469'], # v1 validator keys (gran,babe,imon,para,audi)
+        ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469'], # v2 validator keys (gran,babe,imon,para,asgn,audi)
+        ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469', '62656566'], # v3 validator keys (gran,babe,imon,para,asgn,audi,beef)
+        ['61757261'] # collator keys (aura)
+    )
     possible_prefixes = list(set([j for i in key_formats for j in i]))
-
     if os.path.isdir(dir):
         os.chdir(dir)
         files = os.listdir('.')
-        files = [i for i in files if len(i) == 72 and i[0:8] in possible_prefixes]
+        files = [i for i in files if len(i) in [72, 74] and i[0:8] in possible_prefixes]
         if not files:
             return None
-        # find creation time of the newlest key
+        # find creation time of the newest key
         time_of_last_key = sorted(list(set([int(os.path.getmtime(i)) for i in files])))[-1]
-        # parse the newest public keys and them prefixes from names of files.
-        # creation time can have 1 second drift in theory
-        keys = {i[0:8]: i[8:] for i in files if int(os.path.getmtime(i)) in [time_of_last_key - 1, time_of_last_key, time_of_last_key + 1]}
+        # parse the newest public keys and prefix them with the names of files.
+        # make sure to only pick up the keys created within 60 seconds interval
+        keys = {i[0:8]: i[8:] for i in files if int(os.path.getmtime(i)) <= time_of_last_key and int(os.path.getmtime(i)) > time_of_last_key - 60}
         logger.debug('keys were found: ' + str(keys) + ' in the keystore path: ' + dir)
         for key_format in key_formats:
             if set(keys.keys()) == set(key_format):
@@ -144,7 +146,7 @@ def parse_session_key(dir):
                 session_key = '0x' + ''.join([keys[i] for i in key_format])
                 logger.debug('the session key was parsed: ' + session_key + ' in the keystore path: ' + dir)
                 return(session_key)
-        logger.error('Error of session key parsing')
+        logger.error('Error parsing the session key')
     return None
 
 
